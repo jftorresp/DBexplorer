c2b940873e2a804ccf11ed3f33b1848523f9c6bf
Juan Felipe Torres <jf.torresp@uniandes.edu.co>
2020-03-06 21:36:09 -0500

    Create form view fixed, remains the post

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -25,6 +25,7 @@ selectDbs.addEventListener("change", evt => {
 colSelect.addEventListener("change", evt => {
   const dbName = selectDbs.value;
   const cols = colSelect.value;
+  let llaves = [];
   fetch(`/data/${dbName}/${cols}`)
     .then(res => res.json())
     .then(data => {
@@ -35,22 +36,27 @@ colSelect.addEventListener("change", evt => {
         const daticos = JSON.stringify(d);
         const datoReal = JSON.parse(daticos);
         const array = Object.entries(datoReal);
-        const label = document.createElement("label");
-        const dataInput = document.createElement("input");
-        console.log("array", array);
+        const llavs = Object.keys(datoReal);
         for (let [key, value] of array) {
           const dataRow = document.createElement("div");
           dataRow.className = "row dataDiv";
           dataRow.textContent = `${key} : ${value}`;
           dataDiv.appendChild(dataRow);
-          label.textContent = key;
         }
-        dataForm.appendChild(dataInput);
-        dataForm.appendChild(label);
+        llaves = llavs.slice();
       });
+      for (let llave of llaves) {
+        const label = document.createElement("label");
+        const dataInput = document.createElement("input");
+        label.textContent = llave;
+        dataInput.className = "form-control";
+        dataForm.appendChild(label);
+        dataForm.appendChild(dataInput);
+      }
       button.textContent = "Add";
       dataForm.appendChild(button);
     });
+  
   evt.preventDefault();
 });
 
